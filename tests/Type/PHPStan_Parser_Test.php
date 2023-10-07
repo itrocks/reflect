9d64388f69975d830a92ec41d626d09048b3064b
Baptiste Pillot <baptiste@pillot.fr>
2023-10-07 18:55:58 +0200

    TST I21 PHPStan type parser 'non-negative-int[][]' test

--- tests/Type/PHPStan_Parser_Test.php
+++ tests/Type/PHPStan_Parser_Test.php
@@ -5,6 +5,7 @@ use ITRocks\Reflect\Reflection_Property;
 use ITRocks\Reflect\Type\PHP\Named;
 use ITRocks\Reflect\Type\PHPStan\Collection;
 use ITRocks\Reflect\Type\PHPStan\Parser;
+use ITRocks\Reflect\Type\Undefined;
 use PHPUnit\Framework\TestCase;
 
 class PHPStan_Parser_Test // phpcs:ignore
@@ -20,12 +21,29 @@ class PHPStan_Parser_Test // phpcs:ignore
 	{
 		/** @noinspection PhpUnhandledExceptionInspection exists */
 		$parser = new Parser(new Reflection_Property($this, 'array'), self::class . '[]');
-		$type = $parser->parse();
+		$type   = $parser->parse();
 		self::assertInstanceOf(Collection::class, $type);
 		self::assertEquals(1, $type->getDimensions());
 		$element = $type->getElementType();
 		self::assertInstanceOf(Named::class, $element);
 		self::assertEquals(self::class, $element->getName());
+
+		/** @noinspection PhpUnhandledExceptionInspection exists */
+		$parser = new Parser(new Reflection_Property($this, 'array'), 'non-negative-int[][]');
+		$type   = $parser->parse();
+		self::assertInstanceOf(Collection::class, $type);
+		self::assertEquals(2, $type->getDimensions());
+		$element = $type->getElementType();
+		self::assertInstanceOf(Named::class, $element);
+		self::assertEquals('non-negative-int', $element->getName());
+
+		/** @noinspection PhpUnhandledExceptionInspection exists */
+		$parser = new Parser(new Reflection_Property($this, 'array'), 'bad-type[][][]');
+		$type   = $parser->parse();
+		self::assertInstanceOf(Collection::class, $type);
+		self::assertEquals(3, $type->getDimensions());
+		$element = $type->getElementType();
+		self::assertInstanceOf(Undefined::class, $element);
 	}
 
 }
